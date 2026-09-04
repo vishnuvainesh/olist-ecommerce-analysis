79d28b80fc77a03dbf8bbf571100eea4c801d80f
VISHNUPRIYAN V <159933581+vishnuvainesh@users.noreply.github.com>
2026-09-04 09:31:33 +0530

    Update database.py

--- streamlit/database.py
+++ streamlit/database.py
@@ -1,13 +1,11 @@
-import os
 import mysql.connector
 
 
 def get_connection():
-
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        port=int(os.getenv("DB_PORT", 3306)),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME", "olist_ecommerce")
+        host="localhost",
+        port=3306,
+        user="root",
+        password="Vishnu2005v",
+        database="olist_ecommerce"
     )
